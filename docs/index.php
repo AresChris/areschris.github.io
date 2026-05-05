9e464e42dc1ba6bfe64aeb1ad26d4645a6374608
ARES Christopher <CAres@arfp.asso.fr>
2026-05-05 09:40:23 +0200

    remodif index

--- docs/index.php
+++ docs/index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
 <?php
 get_header();
 if(have_posts()): // si l'url appelé correspond à du contenu (article, page, auteur, catégorie...)
@@ -14,3 +23,8 @@ if(have_posts()): // si l'url appelé correspond à du contenu (article, page, a
 endif;
 
 get_footer();
+
+?>
+
+</body>
+</html>
